bd3330792b91adaa399bad50e8fedc38fcd9f164
kumargautam02 <141772614+kumargautam02@users.noreply.github.com>
2024-12-18 03:51:41 +0530

    Update smtp_mail.py

--- smtp_mail.py
+++ smtp_mail.py
@@ -8,7 +8,7 @@ from jira import JIRA
 # Specify a server key. It should be your
 # domain name link. yourdomainname.atlassian.net
 jiraOptions = {'server': "https://kumargautam41098.atlassian.net"}
-api_token = "ATATT3xFfGF0kFwTbrHUbkWYKhBR3qEiNREAgXdwPbh2rB-QWb4Zt3YgiD0WHgMzeHdtlIzeQqmAYPHXd_rvwAQZA-rdSRnB_SH9VyHagzVJAlPyQvwJWDN9jIC5GcDxNouDpFqEwpu7zT1jvTFUvufWaHMv3DxBd5NmuxCqZHlerru1vuSdw84=B212E125"
+
 # Get a JIRA client instance, pass,
 # Authentication parameters
 # and the Server name.
